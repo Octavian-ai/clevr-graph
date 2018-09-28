f4fc52eeaa9970b45aaf2fa77b3a176ae732e503
David Mack <david@sketchdeck.com>
2018-09-28 13:33:42 -0700

    tidy up script

--- generate-station-properties.sh
+++ generate-station-properties.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -m gqa.generate --count 100000 --small --type-string-prefix StationProperty --name gqa-sp-small-100k
+python -m gqa.generate --count 100 --small --type-string-prefix StationProperty --name sp-small-100k
